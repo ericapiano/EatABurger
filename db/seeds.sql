9d9d94778fe9d298756ce3901d14e362ebe2a459
Erica Piano <ericapiano@Ericas-MacBook-Pro.local>
2018-11-06 17:09:47 -0500

    workinggit add -A

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('McDonalds');
+INSERT INTO burgers (name) VALUES ('Burger King');
+INSERT INTO burgers (name, devoured) VALUES ('Five Guys', true);
+INSERT INTO burgers (name, devoured) VALUES ('Smashburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Habit Burger', true);
+INSERT INTO burgers (name) VALUES ('Wendys');
