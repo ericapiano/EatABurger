5a967a63c6ad35b0b8b5e2e194e79ac999c00b12
Erica Piano <ericapiano@Ericas-MacBook-Pro.local>
2018-11-06 20:10:16 -0500

    heroku updated

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  user: "kr4jce5zomqy0ahi",
+  password: "lqgcq2bpbukmaghy",
+  database: "ajg27yflnmlky2gq"
 });
 
 // Make connection.
